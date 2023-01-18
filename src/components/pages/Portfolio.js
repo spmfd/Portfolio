1f761896bd22b8642cbb0b7d4621b8267bb63aee
spmfd <110492974+spmfd@users.noreply.github.com>
2023-01-17 17:13:28 -0800

    Worked on Contact Page and got hover to work on cards

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './styles/style.css'
 const ceecee = require('./assets/ceecee.png');
 const eatnsip = require('./assets/eatNsip.png');
@@ -7,6 +7,22 @@ const planner = require('./assets/schedule.png');
 const password = require('./assets/password.png');
 
 export default function Portfolio() {
+  
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  const linksStyle = {
+    display: isHover ? 'block' : 'none',
+    filter: isHover ? 'blur(0)' : 'blur(0)'
+  }
+
   return (
     <div class="page">
     <main class="flex-container">
@@ -17,11 +33,11 @@ export default function Portfolio() {
 
     <aside class="main-content">
       <div class="card-grid"> 
-          <div class="card">
+          <div class="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div class="card-container">
           <img src={ceecee} alt="Salon"></img>
               <div class="centered">
-              <div class="links">
+              <div class="links" style={linksStyle}>
                 <h5 class="card-title">Full Stack Website</h5>
                 <a href="https://arcane-mesa-34792.herokuapp.com/" class="card-link" id="card-text">Cee Cee's Salon</a>
                 <a href="https://github.com/blu3bloodedcod3r/CeeCee-s_Salon" class="card-link" id="card-text">GitHub Repo</a>
@@ -30,12 +46,12 @@ export default function Portfolio() {
               </div>
           </div>
 
-          <div class="card" >
+          <div class="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div class="card-container">
           <img src={eatnsip} alt="Food and Drink"></img>
             <div class="card-body">
             <div class="centered">
-              <div class="links">
+              <div class="links" style={linksStyle}>
               <h5 class="card-title">API Query</h5>
               <a href="https://gulogulo208.github.io/eatnsip/" class="card-link">EatNSip</a>
               <a href="https://github.com/gulogulo208/eatnsip" class="card-link">GitHub Repo</a>
@@ -45,12 +61,12 @@ export default function Portfolio() {
             </div>
           </div>
 
-          <div class="card" >
+          <div class="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <div class="card-container">
           <img src={weather} alt="Weather"></img>
             <div class="card-body">
             <div class="centered">
-              <div class="links">
+              <div class="links" style={linksStyle}>
               <h5 class="card-title">API Query</h5>
               <a href="https://github.com/spmfd/Check-the-Weather" class="card-link">Check the Weather</a>
               <a href="https://spmfd.github.io/Check-the-Weather/" class="card-link">GitHub Repo</a>
@@ -60,12 +76,12 @@ export default function Portfolio() {
             </div>
           </div>
 
-          <div class="card" >
+          <div class="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <div class="card-container">
           <img src={planner} alt="Daily Planner"></img>
             <div class="card-body">
             <div class="centered">
-            <div class="links">
+            <div class="links" style={linksStyle}>
               <h5 class="card-title">Local Storage/API</h5>
               <a href="https://github.com/spmfd/What-to-do-today" class="card-link">Daily Planner</a>
               <a href="https://spmfd.github.io/What-to-do-today/" class="card-link">GitHub Repo</a>
@@ -75,12 +91,12 @@ export default function Portfolio() {
             </div>
           </div>
 
-          <div class="card" >
+          <div class="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <div class="card-container">
           <img src={password} alt="PW Generator"></img>
             <div class="card-body">
             <div class="centered">
-            <div class="links">
+            <div class="links" style={linksStyle}>
               <h5 class="card-title">JavaScript Generator</h5>
               <a href="https://spmfd.github.io/Need-a-password-/" class="card-link">Password Generator</a>
               <a href="https://github.com/spmfd/Need-a-password-" class="card-link">GitHub Repo</a>
