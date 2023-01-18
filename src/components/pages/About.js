10c422c9e211816a748f3c7f6f600fd566abd5c6
spmfd <110492974+spmfd@users.noreply.github.com>
2023-01-17 18:05:08 -0800

    Forgot to save About Me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,7 +12,7 @@ export default function About() {
 
 <aside class="main-content">
     <p>
-        Thank you for coming to my portfolio page! I appreciate your interest in my portfolio showcasing my various projects. Not only will they show my efficiency with coding, it serves as proof of my quick learning capabilities. I have been able to go from not knowing any coding to making this website in 6 months!
+      Thank you for coming to my portfolio page! I appreciate your interest in my portfolio showcasing my various projects. Not only will they show my efficiency with coding, it serves as proof of my quick learning capabilities. I have been able to go from not knowing any coding to making this website in 6 months!
       <br></br>
       <br></br>
        Prior to coding a majority of my experience is customer service and banking. I have been a lead to many different projects within the banking industry from AI automation, training, and creating a workflow for an entire department.
